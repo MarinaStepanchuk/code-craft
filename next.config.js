3f977f94bdc9f34e7e67b363f4a62a153eec04d1
MarinaStepanchuk <106726117+MarinaStepanchuk@users.noreply.github.com>
2023-05-24 19:36:11 +0300

    added body styles render on server;

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     NEXTAUTH_URL: 'http://localhost:3000',
     GOOGLE_CLIENT_ID: '595028946356-fa0uleo17lpjjnuhf6o2bu6607b4cuhj.apps.googleusercontent.com',
     GOOGLE_CLIENT_SECRET: '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',
-    GITHUB_ID: ' Iv1.b86ccebf83a5caae',
+    GITHUB_ID: 'Iv1.b86ccebf83a5caae',
     GITHUB_SECRET: '728e5412933ef656f7a1c522be3d44d6935d8c3f',
   },
   typescript: {
