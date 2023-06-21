c1cde93715ce7cd0dfe81374a4750a091258cacb
MarinaStepanchuk <106726117+MarinaStepanchuk@users.noreply.github.com>
2023-06-22 00:44:38 +0300

    remove 404 page;

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -55,12 +55,12 @@ export default async function EditPostPage({ params: { id } }: IPageProps): Prom
     });
     const data = await response.json();
 
-    if (!data) {
-      notFound();
-    }
+    // if (!data) {
+    //   notFound();
+    // }
 
     return <ExpandedPost data={data} />;
   } catch (error) {
-    notFound();
+    // notFound();
   }
 }
